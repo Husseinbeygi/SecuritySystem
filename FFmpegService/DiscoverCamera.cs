82938e6cbf497160265b802a9a324caa542b8298
HusseinBeygi <husseinbeygi.dev@gmail.com>
2021-12-13 11:10:33 +0330

    Added Auto Onvif Camera Discovery

---
+++ FFmpegService/DiscoverCamera.cs
@@ -0,0 +1,16 @@
+﻿using OnvifDiscovery;
+using OnvifDiscovery.Models;
+
+namespace FFmpegService
+{
+    public class DiscoverCamera
+    {
+        public static async Task<IEnumerable<DiscoveryDevice>> OnvifCamera()
+        {
+            var onvifDiscovery = new Discovery();
+            var onvifDevices = await onvifDiscovery.Discover(1);
+            return onvifDevices;
+        }
+    }
+
+}
