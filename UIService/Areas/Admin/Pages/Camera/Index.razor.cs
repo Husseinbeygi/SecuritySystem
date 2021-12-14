15f7056f425d2755e49addd85e84935c56b6f838
HusseinBeygi <husseinbeygi.dev@gmail.com>
2021-12-14 09:08:01 +0330

    Added The Index for list of Ipcameras

--- UIService/Areas/Admin/Pages/Camera/Index.razor.cs
+++ UIService/Areas/Admin/Pages/Camera/Index.razor.cs
@@ -1,6 +1,14 @@
-﻿namespace UIService.Areas.Admin.Pages.Camera
+﻿using SecurityService.Application.Service.Dtos.Camera.IPCamera;
+
+namespace UIService.Areas.Admin.Pages.Camera
 {
+
     public partial class  Index
     {
+        private IPCameraSearchModel searchModel = new IPCameraSearchModel();
+
+        public Index()
+        {
+        }
     }
 }
