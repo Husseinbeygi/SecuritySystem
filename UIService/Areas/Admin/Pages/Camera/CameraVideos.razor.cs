9b20cf0fd121b1e36309129b73c506daa0f086a9
HusseinBeygi <husseinbeygi.dev@gmail.com>
2021-12-22 14:37:19 +0330

    Make CameraPlayer Sing Component

---
+++ UIService/Areas/Admin/Pages/Camera/CameraVideos.razor.cs
@@ -0,0 +1,17 @@
+﻿using SecurityService.Application.Service.Dtos.Camera.IPCamera;
+
+namespace UIService.Areas.Admin.Pages.Camera
+{
+    public partial class CameraVideos
+    {
+        public EditIPCamera cam { get; set; } = new();
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            cam = _camApplication.GetDetails(4);
+
+        }
+
+
+    }
+}
